f128828322d771cf3298778c0aa5fc6af1aefcf0
md mehedi hassan <mdmehedi01326018868@gmail.com>
2025-11-21 22:59:38 +0600

    added post get

--- index.js
+++ index.js
@@ -45,25 +45,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
-    const db=client.db('zap_shift_db');
-    const parcelsCollection=db.collection('parcels');
+
+    const db = client.db("zap_shift_db");
+    const parcelsCollection = db.collection("parcels");
 
     // parcel api
-    app.get('/parcels',async(req,res)=>{
+    app.get("/parcels", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
+      //
+      if (email) {
+        query.senderEmail = email;
+      }
 
-    })
+      const cursor = parcelsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.post('/parcels',async(req,res)=>{
-        const parcel=req.body;
-        const result = await parcelsCollection.insertOne(parcel);
-        res.send(result);
-    })
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      const result = await parcelsCollection.insertOne(parcel);
+      res.send(result);
+    });
 
-     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment , You successfully connected to MongoDB !"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
